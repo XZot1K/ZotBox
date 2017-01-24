af6545d134bcae1bccd0e683bb2338d241424d36
XZot1K <alexosborne54@gmail.com>
2017-01-23 22:12:21 -0500

    * Added some new methods

--- src/XZot1K/plugins/zl/libraries/GeneralLibrary.java
+++ src/XZot1K/plugins/zl/libraries/GeneralLibrary.java
@@ -3,6 +3,7 @@ package XZot1K.plugins.zl.libraries;
 import XZot1K.plugins.zl.Manager;
 import XZot1K.plugins.zl.ZotLib;
 import org.bukkit.ChatColor;
+import org.bukkit.plugin.Plugin;
 
 public class GeneralLibrary
 {
@@ -10,7 +11,7 @@ public class GeneralLibrary
     private ZotLib plugin = Manager.getPlugin();
 
     /**
-     * Color codes the given text
+     * Color codes the given text.
      *
      * @param msg The message you want colored.
      * @return The message color coded.
@@ -27,6 +28,15 @@ public class GeneralLibrary
     public void sendConsoleMessage(String message)
     {
         plugin.getServer().getConsoleSender().sendMessage(color(plugin.getPrefix() + message));
+    }
+
+    /**
+     * @param plugin  Your plugin instance.
+     * @param message The message you wish to send (You must add your own prefix).
+     */
+    public void sendConsoleMessage(Plugin plugin, String message)
+    {
+        plugin.getServer().getConsoleSender().sendMessage(color(message));
     }
 
 }
--- src/XZot1K/plugins/zl/libraries/inventorylib/InventoryLibrary.java
+++ src/XZot1K/plugins/zl/libraries/inventorylib/InventoryLibrary.java
@@ -3,6 +3,7 @@ package XZot1K.plugins.zl.libraries.inventorylib;
 import XZot1K.plugins.zl.Manager;
 import XZot1K.plugins.zl.ZotLib;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
@@ -13,21 +14,46 @@ public class InventoryLibrary
     private ZotLib plugin = Manager.getPlugin();
 
     /**
-     * Remove a specific item from an inventory based on the itemstack's amount.
+     * Remove a specific item from an inventory.
      *
      * @param inventory The inventory you want to remove the item from.
      * @param itemStack The itemstack you want to remove.
      */
-    private void removeItem(Inventory inventory, ItemStack itemStack)
+    public void removeItem(Inventory inventory, ItemStack itemStack, int amount)
     {
-        inventory.removeItem(itemStack);
+        int left = amount;
+
+        for (int i = 0; i < inventory.getSize(); i++)
+        {
+            ItemStack is = inventory.getItem(i);
+
+            if (is != null && doItemsMatch(is, itemStack))
+            {
+                if (left >= is.getAmount())
+                {
+                    inventory.clear(i);
+                    left -= is.getAmount();
+                } else
+                {
+                    if (left <= 0) break;
+                    is.setAmount(is.getAmount() - left);
+                    left = 0;
+                }
+            }
+        }
+
+        if (inventory.getHolder() instanceof Player)
+        {
+            ((Player) inventory.getHolder()).updateInventory();
+        }
     }
 
     /**
      * Creates an new CustomItem, which is used to create new ItemStacks /w meta at ease.
+     *
      * @param materialName The material name you want the item to be.
-     * @param amount The amount you want the stack size to be.
-     * @param durability The durability you want the item to be.
+     * @param amount       The amount you want the stack size to be.
+     * @param durability   The durability you want the item to be.
      * @return A new CustomItem object.
      */
     public CustomItem createCustomItemStack(String materialName, int amount, short durability)
@@ -37,8 +63,9 @@ public class InventoryLibrary
 
     /**
      * Creates an new CustomItem, which is used to create new ItemStacks /w meta at ease.
-     * @param material The material you want the item to be.
-     * @param amount The amount you want the stack size to be.
+     *
+     * @param material   The material you want the item to be.
+     * @param amount     The amount you want the stack size to be.
      * @param durability The durability you want the item to be.
      * @return A new CustomItem object.
      */
@@ -49,8 +76,9 @@ public class InventoryLibrary
 
     /**
      * Creates an new CustomItem, which is used to create new ItemStacks /w meta at ease.
+     *
      * @param materialId The material id you want the item to be.
-     * @param amount The amount you want the stack size to be.
+     * @param amount     The amount you want the stack size to be.
      * @param durability The durability you want the item to be.
      * @return A new CustomItem object.
      */
@@ -61,9 +89,10 @@ public class InventoryLibrary
 
     /**
      * Creates a new CustomInventory, which is used to make a new Inventory (Custom GUI) with ease.
+     *
      * @param inventoryHolder The inventory holder you want to hold the inventory.
-     * @param inventoryTitle The title of the inventory (Color Codes Enabled).
-     * @param inventorySize The size you want the inventory to be (Must be divisible by 9 and less than or equal to 54).
+     * @param inventoryTitle  The title of the inventory (Color Codes Enabled).
+     * @param inventorySize   The size you want the inventory to be (Must be divisible by 9 and less than or equal to 54).
      * @return A new CustomInventory object.
      */
     public CustomInventory createCustomInventory(InventoryHolder inventoryHolder, String inventoryTitle, int inventorySize)
@@ -83,49 +112,69 @@ public class InventoryLibrary
     }
 
     /**
+     * Returns if the given inventory is empty or not.
+     *
+     * @param inventory Inventory you want to check.
+     * @return If the inventory is empty.
+     */
+    public boolean isEmpty(Inventory inventory)
+    {
+        for (int i = 0; i < inventory.getSize(); i++)
+        {
+            ItemStack slot = inventory.getItem(i);
+            if (slot != null || slot.getType() != Material.AIR)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
      * The "doItemsMatch" method allows you to check if two item stacks match EXACTLY (This also works with no item meta).
+     *
      * @param item1 The first item you want to compare.
      * @param item2 The second item you want to compare.
      * @return Whether the two items are the same (Checks ItemMeta if exists).
      */
     public boolean doItemsMatch(ItemStack item1, ItemStack item2)
     {
-        if (item1.hasItemMeta() && item2.hasItemMeta())
+        if (item1.getType() == item2.getType() && item1.getDurability() == item2.getDurability())
         {
-            if (item1.getItemMeta().hasDisplayName() && item2.getItemMeta().hasDisplayName())
+            if (item1.hasItemMeta() && item2.hasItemMeta())
             {
-                if (item1.getItemMeta().getDisplayName().equals(item2.getItemMeta().getDisplayName()))
+                if (item1.getItemMeta().hasDisplayName() && item2.getItemMeta().hasDisplayName())
                 {
-                    if (item1.getItemMeta().hasLore() && item2.getItemMeta().hasLore())
+                    if (item1.getItemMeta().getDisplayName().equals(item2.getItemMeta().getDisplayName()))
                     {
-                        if (item1.getItemMeta().getLore().equals(item2.getItemMeta().getLore()))
+                        if (item1.getItemMeta().hasLore() && item2.getItemMeta().hasLore())
+                        {
+                            if (item1.getItemMeta().getLore().equals(item2.getItemMeta().getLore()))
+                            {
+                                return true;
+                            }
+                        } else if (!item1.getItemMeta().hasLore() && !item2.getItemMeta().hasLore())
                         {
                             return true;
                         }
-                    } else if (!item1.getItemMeta().hasLore() && !item2.getItemMeta().hasLore())
-                    {
-                        return true;
                     }
-                }
-            } else if (!item1.getItemMeta().hasDisplayName() && !item2.getItemMeta().hasDisplayName())
-            {
-                if (item1.getItemMeta().getDisplayName().equals(item2.getItemMeta().getDisplayName()))
+                } else if (!item1.getItemMeta().hasDisplayName() && !item2.getItemMeta().hasDisplayName())
                 {
-                    if (item1.getItemMeta().hasLore() && item2.getItemMeta().hasLore())
+                    if (item1.getItemMeta().getDisplayName().equals(item2.getItemMeta().getDisplayName()))
                     {
-                        if (item1.getItemMeta().getLore().equals(item2.getItemMeta().getLore()))
+                        if (item1.getItemMeta().hasLore() && item2.getItemMeta().hasLore())
+                        {
+                            if (item1.getItemMeta().getLore().equals(item2.getItemMeta().getLore()))
+                            {
+                                return true;
+                            }
+                        } else if (!item1.getItemMeta().hasLore() && !item2.getItemMeta().hasLore())
                         {
                             return true;
                         }
-                    } else if (!item1.getItemMeta().hasLore() && !item2.getItemMeta().hasLore())
-                    {
-                        return true;
                     }
                 }
-            }
-        } else
-        {
-            if (item1.getType() == item2.getType() && item1.getDurability() == item2.getDurability())
+            } else
             {
                 return true;
             }
