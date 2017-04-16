3bed1d4d9c9441c2d00ccbb100f9d02f4dc47f09
XZot1K <alexosborne54@gmail.com>
2017-04-15 20:34:31 -0400

    * Added JavaDoc comments to the InventoryLibrary. * Redid some small methods in the InventoryLibrary. * Added the getYaw() method to the InventoryLibrary.

--- src/XZot1K/plugins/zb/libraries/locationlib/LocationLibrary.java
+++ src/XZot1K/plugins/zb/libraries/locationlib/LocationLibrary.java
@@ -2,9 +2,11 @@ package XZot1K.plugins.zb.libraries.locationlib;
 
 import XZot1K.plugins.zb.ZotBox;
 import org.bukkit.Chunk;
+import org.bukkit.Effect;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Entity;
 
 import java.util.ArrayList;
@@ -20,6 +22,15 @@ public class LocationLibrary
 
     private ZotBox plugin = ZotBox.getInstance();
 
+    /**
+     * Gets all blocks in a radius around the fed center location.
+     *
+     * @param centerPoint The location to get all blocks around.
+     * @param xRadius     The X radius.
+     * @param yRadius     The Y Radius.
+     * @param zRadius     The Z Radius.
+     * @return A list of all the blocks found.
+     */
     public List<Block> getBlocksInRadius(Location centerPoint, int xRadius, int yRadius, int zRadius)
     {
         List<Block> blocks = new ArrayList<>();
@@ -37,6 +48,14 @@ public class LocationLibrary
         return blocks;
     }
 
+    /**
+     * Replaces all blocks in the fed list with the specified material and data to the new material and data.
+     * @param blocksList The list of blocks to replace.
+     * @param fromMaterial The material that a block has in the block list.
+     * @param toMaterial The material that you want the blocks to change to.
+     * @param fromData The data that a block has in the block list.
+     * @param toData The data that you want the blocks to change to.
+     */
     public void replaceBlocks(List<Block> blocksList, Material fromMaterial, Material toMaterial, byte fromData, byte toData)
     {
         for (int i = -1; ++i < blocksList.size(); )
@@ -50,12 +69,24 @@ public class LocationLibrary
         }
     }
 
+    /**
+     * Gets if the defined location is between the 2 specified points. It's just like the well-known plugin WorldEdit.
+     * @param location The location you want to check is between the two locations.
+     * @param point1 The 1st location.
+     * @param point2 The 2nd location.
+     * @return Whether the location is in fact between the two locations or not.
+     */
     public boolean isLocationBetweenTwoLocations(Location location, Location point1, Location point2)
     {
         return location.toVector().isInAABB(point1.toVector(), point2.toVector())
                 || location.toVector().isInAABB(point2.toVector(), point1.toVector());
     }
 
+    /**
+     * Gets all the blocks in the specified chunk.
+     * @param chunk The chunk you want to get all blocks from.
+     * @return A list of all the found blocks.
+     */
     public List<Block> getBlocksInChunk(Chunk chunk)
     {
         List<Block> blocks = new ArrayList<>();
@@ -76,6 +107,12 @@ public class LocationLibrary
         return blocks;
     }
 
+    /**
+     * Gets all entities in a radius around the location within the defined distance.
+     * @param centerLocation The center location to find entities around.
+     * @param distance The distance from the specified center location.
+     * @return A list off all the found entities.
+     */
     public List<Entity> getEntitiesInRadius(Location centerLocation, double distance)
     {
         List<Entity> entities = new ArrayList<>();
@@ -90,7 +127,13 @@ public class LocationLibrary
         return entities;
     }
 
-    public Direction getYawDirection(float yaw)
+    /**
+     * Gets a direction based on the fed yaw value.
+     *
+     * @param yaw The yaw from a location (player or entity to be specific).
+     * @return The direction that was found/calculated.
+     */
+    public Direction getDirection(float yaw)
     {
         double rotation = (yaw - 90.0F) % 360.0F;
         if (rotation < 0.0D)
@@ -117,6 +160,11 @@ public class LocationLibrary
         return null;
     }
 
+    /**
+     * Swaps the two entities locations.
+     * @param entity1 The 1st entity you want to switch with the 2nd.
+     * @param entity2 The 2nd entity you want to switch with the 1st.
+     */
     public void swapEntities(Entity entity1, Entity entity2)
     {
         Location loc1 = entity1.getLocation();
@@ -125,7 +173,13 @@ public class LocationLibrary
         entity1.teleport(loc2);
     }
 
-    public void swapBlocks(Block block1, Block block2)
+    /**
+     * Swaps the 2 fed blocks locations.
+     *
+     * @param block1 The 1st block you want to swap with the 2nd.
+     * @param block2 The 2nd block you want to swap with the 1st.
+     */
+    public void swapBlocks(Block block1, Block block2, boolean useEffects)
     {
         Material block1Material = block1.getType();
         byte block1Data = block1.getData();
@@ -136,6 +190,33 @@ public class LocationLibrary
         block1.setData(block2Data);
         block2.setType(block1Material);
         block2.setData(block1Data);
+
+        if (useEffects)
+        {
+            block1.getWorld().playEffect(block1.getLocation(), Effect.STEP_SOUND, block1.getTypeId());
+            block2.getWorld().playEffect(block2.getLocation(), Effect.STEP_SOUND, block2.getTypeId());
+        }
+    }
+
+    /**
+     * Gets a yaw value based on the block face fed to it.
+     *
+     * @param blockFace The blockface you want to calculate the yaw from.
+     * @return The yaw that was calculated from the blockface.
+     */
+    public double getYaw(BlockFace blockFace)
+    {
+        switch (blockFace)
+        {
+            case EAST:
+                return (float) 90;
+            case SOUTH:
+                return (float) 180;
+            case WEST:
+                return (float) 270;
+            default:
+                return (float) 0;
+        }
     }
 
 }
